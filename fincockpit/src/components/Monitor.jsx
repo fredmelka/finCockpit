6c963cac6f04037a428c98cb2519a21d1bbd926d
fredmelka <fred.melka@gmail.com>
2023-05-15 00:10:14 +0200

    still building the Monitor..

--- fincockpit/src/components/Monitor.jsx
+++ fincockpit/src/components/Monitor.jsx
@@ -5,41 +5,40 @@ import axios from 'axios';
 import { Button, Skeleton, Space, Table, Tag } from 'antd';
 
 const urlFinnhubCompanyProfile2 = 'https://finnhub.io/api/v1/stock/profile2?symbol=';
-const urlFinnhubCompanyRecommendation = 'https://finnhub.io/api/v1/stock/recommendation?symbol=';
 const tokenFinnhub = 'chdons9r01qk9rb2m89gchdons9r01qk9rb2m8a0';
 
 
 export default function Monitor({securitiesList}) {
-
+    
+    console.log('liste de stocks a traiter', securitiesList);
+    
 let [data, setData] = useState([]);
 
-// Asynchronous function with concurrency of actions
 // Function that builds Datasource required to render the <Table> antd Component
-async function getData(securitiesList) {
+async function getData() {
 
-for (let i in securitiesList) {
-
-console.log(i, securitiesList[i]);
-
-    try {
-        let responseProfile2 = axios.get(`${urlFinnhubCompanyProfile2}${securitiesList[i]}&token=${tokenFinnhub}`);
-        let responseRecommendation = axios.get(`${urlFinnhubCompanyRecommendation}${securitiesList[i]}&token=${tokenFinnhub}`);
-        let profileInfo = (await responseProfile2).data;
-        let recommendation = (await responseRecommendation).data;
-
-        let securityData = Object.assign(profileInfo, recommendation[0]);
-        securityData.key = i;
-        console.log(securityData);
-        setData((data) => [...data, securityData]);
-    }
-        
-    catch (error) {console.log(error)};
+try {
+    let info = [];
+    for (let i=0; i < securitiesList.length; i++) {
+            
+        let responseProfile2 = await axios.get(`${urlFinnhubCompanyProfile2}${securitiesList[i]}&token=${tokenFinnhub}`);
+        let securityData = responseProfile2.data;
+        if (Object.keys(securityData).length > 0) {securityData.key = i; info = [...info, securityData];};};
+    setData(info);}
+    
+catch (error) {console.log(error)};
 };
-console.log('datasource:', data);
+    
+useEffect(() => {console.log('effect ran'); getData();}, [securitiesList]);
+    
+console.log(data);
+    
+return (<><div>{`Salut ! ${data.length}`}</div></>);
 };
 
-useEffect(() => {console.log('effect ran'); getData(securitiesList)}, [securitiesList]);
+// const urlFinnhubCompanyRecommendation = 'https://finnhub.io/api/v1/stock/recommendation?symbol=';
 
-
-return (<><div>Salut</div></>);
-};
+// Asynchronous function with potential concurrency of actions if the below API call is added
+// let responseRecommendation = axios.get(`${urlFinnhubCompanyRecommendation}${securitiesList[i]}&token=${tokenFinnhub}`);
+// let recommendation = (await responseRecommendation).data;
+// let securityData = Object.assign(profileInfo, recommendation[0]);
